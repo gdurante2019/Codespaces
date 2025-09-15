631f4e2d7f66ac0e57910bf4d87a7c0374c3ad80
Gina Durante <46698605+gdurante2019@users.noreply.github.com>
2025-09-15 01:43:44 +0000

    modify ping.py

--- ping.py
+++ ping.py
@@ -1,12 +1,12 @@
-import uvicorn
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI(title="ping")
 
 @app.get("/ping")
 
 def ping():
-    print("Pong!")
+    return "Pong!"
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0")
+    uvicorn.run(app, host="0.0.0.0", port=9696)
